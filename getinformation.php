8cec695bc9395e5e219036b41e4436e1327b6606
Cescteruel <Cescteruel@gmail.com>
2019-02-26 13:30:12 +0100

    Update getinformation.php

--- getinformation.php
+++ getinformation.php
@@ -1,9 +1,9 @@
 <script>
   localStorage.setItem('translate', JSON.stringify({
 		<?php
-			$apptranslate = mysqli_query( $link, "select * from est_apptranslate" );
+			$apptranslate = mysqli_query( $link, "select * from apptranslate" );
 			while ( $apptranslate_row = mysqli_fetch_array( $apptranslate )){
-					echo '"'.$apptranslate_row["tagkey"].'" : "'.$apptranslate_row["lang_".$idiomamail.""].'",';
+					echo '"'.$apptranslate_row["tagkey"].'" : "'.$apptranslate_row["lang_es"].'",';
 			}
 		?>
 	}));
